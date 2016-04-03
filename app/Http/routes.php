83789d7f7d440661424941f2ed2fd74835c9c530
Joe Ferguson <joe@joeferguson.me>
2016-04-03 15:33:48 -0500

    diable routes we have moved functionality to the results route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,21 +23,21 @@ Route::get('/results', [
     'as' => 'results', 'uses' => 'ResultsController@queryResults'
 ]);
 
-Route::get('/self-employed', [
-    'as' => 'self-employed', 'uses' => 'ResultsController@getSelfEmployed'
-]);
+//Route::get('/self-employed', [
+//    'as' => 'self-employed', 'uses' => 'ResultsController@getSelfEmployed'
+//]);
 
-Route::get('/employer-provides-mental-health', [
-    'as' => 'employer-provides-mental-health', 'uses' => 'ResultsController@employerProvidesMentalHealth'
-]);
-
-Route::get('/unknown-coverage-suspect-condition', [
-    'as' => 'unknown-coverage-suspect-condition', 'uses' => 'ResultsController@peopleInUsDontKnowIfTheyHaveCoverageButHaveBeenOrSuspectMentalCondition'
-]);
-
-Route::get('/have-issue-not-sought-treatment', [
-    'as' => 'have-issue-not-sought-treatment', 'uses' => 'ResultsController@peopleWhoHaveMHIssueThatHaveNotSoughtTreatment'
-]);
+//Route::get('/employer-provides-mental-health', [
+//    'as' => 'employer-provides-mental-health', 'uses' => 'ResultsController@employerProvidesMentalHealth'
+//]);
+//
+//Route::get('/unknown-coverage-suspect-condition', [
+//    'as' => 'unknown-coverage-suspect-condition', 'uses' => 'ResultsController@peopleInUsDontKnowIfTheyHaveCoverageButHaveBeenOrSuspectMentalCondition'
+//]);
+//
+//Route::get('/have-issue-not-sought-treatment', [
+//    'as' => 'have-issue-not-sought-treatment', 'uses' => 'ResultsController@peopleWhoHaveMHIssueThatHaveNotSoughtTreatment'
+//]);
 
 //Route::get('/import', [
 //    'as' => 'import', 'uses' => 'ResultsController@importResults'
--- readme.md
+++ readme.md
@@ -17,10 +17,5 @@ Based on Laravel 5.2
 
 Currently Supported Routes
 
-* http://localhost:8080/import - Imports CSV into Results Table, only run ONCE!
-* http://localhost:8000/ - Shows Header row of CSV
+* http://localhost:8000/questions - Shows Header row of CSV
 * http://localhost:8000/results - Shows all currently configured query results
-* http://localhost:8000/employer-provides-mental-health - Shows results based on "Does your employer provide mental health benefits as part of healthcare coverage?"
-* http://localhost:8000/unknown-coverage-suspect-condition - Shows results based on "people in the US, don't know if they have mental health care coverage, but have been or suspect they have a mental health condition"
-* http://localhost:8000/self-employed - Shows results based on "Are you self-employed?"
-* http://localhost:8000/have-issue-not-sought-treatment - People who have Mental Health Issue That Have Not Sought Treatment"
