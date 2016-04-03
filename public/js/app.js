a9ac18b459f9566984c72eed1caaeaeaa1d95811
Joe Ferguson <joe@joeferguson.me>
2016-04-03 15:12:24 -0500

    add preventDefault() to JS toggle

--- public/js/app.js
+++ public/js/app.js
@@ -12216,7 +12216,9 @@ require('bootstrap-sass');
 $(document).ready(function () {
     // console.log($.fn.tooltip.Constructor.VERSION);
 
-    $('a.list-group-item').click(function () {
+    $('a.list-group-item').click(function (e) {
+        e.preventDefault();
+
         if ($(this).children('.answer').hasClass('hidden')) {
             $(this).children('.answer').removeClass('hidden').addClass('visible');
         } else {
